af20fdec7e84482e35a9b7c08db9491b4923d42e
Jacy Mae Espinosa <jaymespinosa10@gmail.com>
2021-11-04 23:20:42 -0700

    Created the get_client_credentials to access Spotify API

--- spotify.py
+++ spotify.py
@@ -1,11 +1,25 @@
 from datetime import datetime, timedelta
+import base64
+import requests
 
-
-client_id = "ENTER CLIENT ID FROM SPOTIFY ACCOUNT"
-client_secret = "ENTER SECRET ID FROM SPOTIFY ACCOUNT"
-
-
+'''Client ID and Client Secret can be retrieved from the user's Spotify account.'''
 class SpotifyAPI(object):
     client_id = None
     client_secret = None
     token_url = "https://accounts.spotify.com/api/token"
+
+    def __init__(self, client_id, client_secret, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.client_id = client_id
+        self.client_secret = client_secret
+
+    def get_client_credentials(self):
+        client_id = self.client_id
+        client_secret = self.client_secret
+
+        if client_secret == None or client_id == None:
+            raise Exception("Set client_id and client_secret")
+
+        client_creds = f"{client_id}:{client_secret}"
+        client_creds_b64 = base64.b64encode(client_creds.encode())
+        return client_creds_b64.decode()
