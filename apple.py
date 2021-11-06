ceda1e33c4bfc3d581c980413e183ef2c8ee3cbd
Jacy Mae Espinosa <jaymespinosa10@gmail.com>
2021-11-05 21:14:32 -0700

    Created a get_authorization method that returns an authorization token

--- apple.py
+++ apple.py
@@ -5,7 +5,9 @@ CLIENT_SECRET = "ENTER CLIENT SECRET"
 '''APPLE'''
 APPLE_KEY = "ENTER APPLE KEY"
 TEAM_ID = "ENTER APPLE TEAM ID"
-
+SECRET_KEY = '''-----BEGIN PRIVATE KEY-----
+ENTER SECRET KEY HERE
+-----END PRIVATE KEY-----'''
 
 class AppleMusic:
 
@@ -13,4 +15,18 @@ class AppleMusic:
         self.apple_key = apple_key
         self.team_id = team_id
         self.secret_key = secret_key
+        self.token = None
 
+    def get_authorization(self):
+        """
+        Get header for API request
+        :return: header in dictionary format
+        """
+        if self.token:
+            return {
+                'Authorization': f'Bearer {self.token}',
+                'Music-User-Token': f'{self.token}'
+            }
+
+        else:
+            return {}
